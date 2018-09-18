df241d662ac42e9ebdd32e1829f3486eec33084c
Keely Weisbeck <keelyweisbeck@gmail.com>
2018-09-18 13:14:58 -0600

    updates to board logic

--- Program1/Board.py
+++ Program1/Board.py
@@ -11,13 +11,26 @@ def print_board(self):
         print(" ".join(row))
 
 
-def update_board(self, x, y):
+def update_board(board, x, y, update):
     file = open("own_board.txt","r")
     f1 = file.readlines()
     #for x in f1:
         #print(x)
     #self[x][y] = "X"
     #return self
+    if update == 1: # hit
+        board[x][y] = 'C' # the letter of ship
+    elif update == 2: # miss
+        board[x][y] = 'M'
+    elif update == 3: # sink
+        board[x][y] = 'S'
+    else:
+        print("UPDATE: something is wrong")
+
+def check_board(board, x, y):
+    if board[x][y] == '_':
+        print("MISS")
+    
 
 def read_board(self):
     return 0
